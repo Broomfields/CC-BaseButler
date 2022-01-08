546ea87f146411b419cf96969a4d9d69a3531667
Shaun Broomfield <36537416+Enstroe@users.noreply.github.com>
2022-01-08 18:04:47 +0000

    Further refactoring and CC specific syntax fixes

--- Winston.lua
+++ Winston.lua
@@ -1,33 +1,50 @@
+------------------------------------------------------
+-- # Title : Winston 
+------------------------------------------------------
+-- # Description : A Modded-Minecraft Automation Assistant 
+-- #             - built in 'Computer Craft'/'CC: Tweaked' and 'Advanced Peripherals'.
+------------------------------------------------------
+
+
+
+---------------------------
+-- # Member Variable Declarations 
+---------------------------
+
 -- Change to direction or find
-monitor = peripheral.find("monitor")
+Monitor = peripheral.find("Monitor")
 
-scale = 1
+Scale = 1
 
-posX = scale
-posY = scale
+PosX = Scale
+PosY = Scale
 
-commandPhrase = "Winston"
-commandPhraseLength = string.len(commandPhrase)
-
-
--- Functions
+CommandUser = "Broomfields"
+CommandPhrase = "Winston"
+CommandPhraseLength = string.len(CommandPhrase)
 
 
--- Initialises a connected monitor
+
+---------------------------
+-- # Functions 
+---------------------------
+
+-- Initialises a connected Monitor
 function InitialiseMonitor()    --Called only by AssertMonitorPresent()
-    monitor.setBackgroundColour(colors.gray)
-    monitor.setTextColour(1)
-    monitor.setTextScale(1)
-    monitor.setCursorPos(1,1)
-    monitor.clear()
+    Monitor.setBackgroundColour(colors.gray)
+    Monitor.setTextColour(1)
+    Monitor.setTextScale(1)
+    Monitor.setCursorPos(1,1)
+    Monitor.clear()
 end
 
 
--- Outputs any given text, and if specified, any give text type to the computer and a connected monitor
-function WriteLine(text, typeText = "")
+-- Outputs any given text, and if specified, any give text type to the computer and a connected Monitor
+function WriteLine(text, typeText)
 
-    print("cursorPosY = " .. posY)
-    if text == nil then
+    print("cursorPosY = " .. PosY)
+
+    if (text == nil) then
         print("Error - WriteLine(text) - text == nil")
     else
 
@@ -38,13 +55,13 @@ function WriteLine(text, typeText = "")
             print(typeText .. " - " .. text)
         end    
 
-        -- Outputs to the monitor if one is present
-        if (AssertMonitorPresent() == true)
+        -- Outputs to the Monitor if one is present
+        if (AssertMonitorPresent() == true) then
 
-            monitor.write(text)
+            Monitor.write(text)
         
-            posY = posY + scale
-            monitor.setCursorPos(posX, posY)
+            PosY = PosY + Scale
+            Monitor.setCursorPos(PosX, PosY)
         end
     end
 end
@@ -56,49 +73,49 @@ function NewLine()
 end
 
 
--- Outputs a formatted message from the program to the computer and a connected monitor
+-- Outputs a formatted message from the program to the computer and a connected Monitor
 function ComputerLine(text)
-    monitor.setTextColour(colors.lightBlue)
+    Monitor.setTextColour(colors.lightBlue)
     WriteLine(text, "Computer")
 end
 
 
--- Outputs a formatted message from chat to the computer and a connected monitor
+-- Outputs a formatted message from chat to the computer and a connected Monitor
 function ChatLine(text)
-    monitor.setTextColour(colors.white)
+    Monitor.setTextColour(colors.white)
     WriteLine(text, "Chat")
 end
 
 
--- Outputs a formatted error message to the computer and a connected monitor
+-- Outputs a formatted error message to the computer and a connected Monitor
 function ErrorLine(text)
-    monitor.setTextColour(colors.red)
+    Monitor.setTextColour(colors.red)
     WriteLine(text, "Error")
 end
 
 
--- Asserts if a monitor is connected, relaying a relative message if this is a new change and initialising a monitor it was just connected
+-- Asserts if a Monitor is connected, relaying a relative message if this is a new change and initialising a Monitor it was just connected
 function AssertMonitorPresent() -- Called in MainProcess()
 
-    local monitorWasPresent = !(monitor == nil)
+    local MonitorWasPresent = (Monitor ~= nil)
 
-    monitor = peripheral.find("monitor")
+    Monitor = peripheral.find("Monitor")
 
-    if (monitor == nil) then
+    if (Monitor ~= nil) then
         
-        if (monitorWasPresent == true) then
-            ErrorLine("Monitor has been disconnected!")
-        end
-
-        return false
-    else
-        
-        if (monitorWasPresent == false) then
+        if (MonitorWasPresent == false) then
             ComputerLine("Monitor has been connected!")
             InitialiseMonitor()
         end
-
         return true
+
+    else
+        
+        if (MonitorWasPresent == true) then
+            ErrorLine("Monitor has been disconnected!")
+        end
+        return false
+
     end
 end
 
@@ -106,23 +123,28 @@ end
 -- Asserts that a command is present in the given text
 function AssertCommand(text)
 
-    if text == nil then
-        print("Error - AssertCommand(text) - text == nil")
-    else
-        if string.len(text) >= commandPhraseLength then
+    if (text ~= nil) then
+
+        if (string.len(text) >= CommandPhraseLength) then
             
-            local comparison = string.sub(text, 1, commandPhraseLength)
+            local comparison = string.sub(text, 1, CommandPhraseLength)
             print("### Command Assertion ###")
             print("Text: '" .. text .. "'")
             print("Subbed Comparison: '" .. comparison .. "'")
-            print("Command Phrase: '" .. commandPhrase .. "'")
+            print("Command Phrase: '" .. CommandPhrase .. "'")
             print("### End Command Assertion ###")
 
-            if comparison == commandPhrase then
+            if (comparison == CommandPhrase) then
                 -- Command Found
                 return true
             end 
+
         end
+
+    else
+
+        print("Error - AssertCommand(text) - text == nil")
+
     end
     
     -- Command Not Found
@@ -136,47 +158,53 @@ function ParseCommand(text)
 end
 
 
+-- Main Process Function (TODO : Turn in to State Machine)
+function MainProcess()
 
--- \\\\\\\\\\\\\\\\\\\\\\\\\-
--- # Start Main Process (Will be moved to a state machine at somepoint)
--- \\\\\\\\\\\\\\\\\\\\\\\\\-
+    local box = peripheral.find("chatBox") -- Finds the peripheral if one is connected
 
-local box = peripheral.find("chatBox") -- Finds the peripheral if one is connected
+    if (box == nil) then
+    ErrorLine("chatBox not found")
+    end
 
-if box == nil then
-   ErrorLine("chatBox not found")
-   
-end
+    ComputerLine("Waiting for messages...")
+    NewLine()
 
-ComputerLine("Waiting for messages...")
-NewLine()
+    local inError = false
+    while inError == false do
 
-local inError = false
-while inError == false do
-
-    local eventData = {os.pullEvent("chat")}
-    local event = eventData[1]
-    local username = eventData[2]
-    local message = eventData[3]
-        
-    ChatLine("<" .. username .. "> " .. message)
-
-    --Change to your username    
-    if username == "Broomfields" then
-        if AssertCommand(message) then
+        local eventData = {os.pullEvent("chat")}
+        local event = eventData[1]
+        local username = eventData[2]
+        local message = eventData[3]
             
-            ComputerLine("### Command Identified ###")
-            box.sendMessage("Command Identified")
-            
-            if ParseCommand(message) then
-                ComputerLine("### Command Parsed ###")
-                box.sendMessage("Command Parsed")
-            else
-                ComputerLine("### Command Not Parsed ### ")
-                box.sendMessage("Command Not Parsed")
+        ChatLine("<" .. username .. "> " .. message)
+
+        --Change to your username    
+        if (username == CommandUser) then
+            if (AssertCommand(message)) then
+                
+                ComputerLine("### Command Identified ###")
+                box.sendMessage("Command Identified")
+                
+                if (ParseCommand(message)) then
+                    ComputerLine("### Command Parsed ###")
+                    box.sendMessage("Command Parsed")
+                else
+                    ComputerLine("### Command Not Parsed ### ")
+                    box.sendMessage("Command Not Parsed")
+                end
             end
         end
     end
+
+    ComputerLine("Terminating Instance.") 
 end
 
-ComputerLine("Terminating Instance.") 
+
+
+---------------------------
+-- # Start Main Process 
+---------------------------
+
+MainProcess()
